4f7b98b032ac6ae685623ecb9ba506563046af80
EsorOne <esorone@gmail.com>
2024-03-23 17:43:37 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if /tmp/.X11-unix/* exists
+if ls /tmp/.X11-unix/*; then
+    echo "Removing /tmp/.X11-unix/..."
+    sudo rm /tmp/.X11-unix/*
+else
+    echo "/tmp/.X11-unix/ does not exist, running VNC server..."
+    sudo vncserver -geometry 1920x1080 :1
+fi
