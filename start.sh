6c287a361ac3f78e2d2b1cf445e9a66aa592dfa3
EsorOne <esorone@gmail.com>
2024-03-24 21:03:25 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 if ls /tmp/.X11-unix/*; then
     echo "Removing /tmp/.X11-unix/..."
     sudo rm /tmp/.X11-unix/*
+    sudo rm -R /tmp/.X1-lock
 else
     echo "/tmp/.X11-unix/ does not exist, running VNC server..."
     sudo vncserver -geometry 1920x1080 :1
