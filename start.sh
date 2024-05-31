7d12747e7d0e11e0d2b59878b8f374a9459e5b6f
EsorOne <esorone@gmail.com>
2024-05-31 19:59:08 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
 
-# Check if /tmp/.X11-unix/* exists
-if ls /tmp/.X11-unix/*; then
-    echo "Removing /tmp/.X11-unix/..."
-    sudo rm /tmp/.X11-unix/*
-    sudo rm -R /tmp/.X1-lock
+file=/tmp/.X1-lock
+
+
+if [ -e "$file" ]; then
+    echo "File exists, so removing X1-Lock"
+    sudo rm /tmp/.X1-lock
+
 else
-    echo "/tmp/.X11-unix/ does not exist, running VNC server..."
+    echo "Running Xtightvnc"
     sudo vncserver -geometry 1920x1080 :1
-fi
+    pidof Xtightvnc
+fi;
+
+dir=/tmp/.X11-unix/
+
+ if [ -d "$dir" ]; then
+    echo "Dir exists, so removing X11-unix"
+      sudo rm /tmp/.X11-unix/*
+
+ elif [ ! -d "$dir" ]; then
+    echo "Dir does exists, so NOT removing X11-unix"
+      sudo rm /tmp/.X11-unix/*
+
+else echo "Next Step and starting server";
+
+fi;
+
+    echo "Running Xtightvnc"
+var1='sudo kill -9'
+var2=`pidof Xtightvnc`
+var4="${var1} ${var2}"
+echo $var4
+$var4
+
+    sudo vncserver -geometry 1920x1080 :1
